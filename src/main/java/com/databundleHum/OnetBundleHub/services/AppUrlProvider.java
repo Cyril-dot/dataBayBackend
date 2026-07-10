3f147834a76c0fab1932621c2d8b10a45e1b9238
Cyril-dot <chukwukaottih@gmail.com>
2026-07-10 17:33:01 +0000

    updaste2

--- src/main/java/com/databundleHum/OnetBundleHub/services/AppUrlProvider.java
+++ src/main/java/com/databundleHum/OnetBundleHub/services/AppUrlProvider.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppUrlProvider {
 
-    @Value("${app.base-url:https://www.databaygh.shop")
+    @Value("${app.base-url:https://www.databaygh.shop}")
     private String baseUrl;
 
     /**
